9d7b55262ef8fd3c169d59e072022803d370ae02
clr1026 <32473401+clr1026@users.noreply.github.com>
2017-10-15 21:58:22 +0800

    Update PriceTick.cs

--- PriceTick.cs
+++ PriceTick.cs
@@ -1,7 +1,7 @@
 using System;
 /*
-				CB				ETF				X
-price_range  [150, 1000]		[50]		[50, 500]
+		CB		ETF		X
+price_range     [150, 1000]	[50]		[50, 500]
 tick_size	[0.05, 1, 5]	[0.01, 0.05]	[0.1, 0.5, 1]
 */
 
